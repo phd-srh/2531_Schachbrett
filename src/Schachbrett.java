24f63d5235fc9262baaf35a8d8901746f8395450
Dr. Dillinger <peter.dillinger@srh.de>
2023-10-31 11:50:00 +0100

    Initial commit

---
+++ src/Schachbrett.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Schachbrett {
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        System.out.print("Startwert: ");
+        int startwert = eingabe.nextInt();
+        System.out.print("Anzahl Zeilen: ");
+        int anzahlDerZeilen = eingabe.nextInt();
+
+        // TODO
+    }
+}
