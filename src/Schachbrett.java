27974eb059d072893058414d0da73a037c7db271
Dr. Dillinger <peter.dillinger@srh.de>
2023-11-02 11:26:01 +0100

    Schachbrett erweitert. Negative Zahlen sind jetzt nicht mehr zugelassen. Ausgabe für max. 4-stellige Zahlen. Immer noch keine endgültige Lösung.

--- src/Schachbrett.java
+++ src/Schachbrett.java
@@ -8,9 +8,22 @@ public class Schachbrett {
         System.out.print("Anzahl Zeilen: ");
         int anzahlDerZeilen = eingabe.nextInt();
 
+        // Wächteranweisung (Türsteher)
+        if (startwert < 0) {
+            System.out.println("Sorry, mit dem Startwert komme ich nicht klar.");
+            return;
+        }
+        int maximalerAusgabeWert = startwert + (anzahlDerZeilen - 1) * 2;
+
         for (int zeile = startwert; zeile < startwert + anzahlDerZeilen; zeile++) {
             for (int spalte = zeile; spalte < zeile + anzahlDerZeilen; spalte++) {
-                if (spalte < 10) {
+                if (spalte < 10 && maximalerAusgabeWert >= 10) {
+                    System.out.print(" ");
+                }
+                if (spalte < 100 && maximalerAusgabeWert >= 100) {
+                    System.out.print(" ");
+                }
+                if (spalte < 1000 && maximalerAusgabeWert >= 1000) {
                     System.out.print(" ");
                 }
                 System.out.print(spalte + " ");
